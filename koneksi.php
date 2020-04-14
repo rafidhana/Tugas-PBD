cbe3ca8355b013ee1184c027bdbf699a451075b4
Rafi Dhana <55130515+rafidhana@users.noreply.github.com>
2020-04-15 02:18:20 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-function open_connection() {
-	$hostname = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "akademik63";
-	$koneksi = mysqli_connect($hostname, $username, $password, $dbname);
-	return $koneksi;
-	}  ?>
